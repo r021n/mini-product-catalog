e2023b70df9a4dd71ed47476fe48fc16ef7af095
r021n <rozinabdul25@gmail.com>
2026-02-11 22:17:25 +0700

    feat: Add `Category` and `CategoryCreateRequest` models for managing product categories.

---
+++ backend/internal/model/category.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CategoryCreateRequest struct {
+	Name string `json:"name" validate:"required,min=2,max=50"`
+}
